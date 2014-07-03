3ecfae8ce478808a2a11fde1302654c30620853f
mrich137 <matt.rich@banctec.com>
2014-07-03 17:47:48 -0500

    Rename reader/writer to generics

--- src/AdminCleaner.py
+++
@@ -1,5 +0,0 @@
-
-
-# Used to:
-#	* Move Converted .ack files to Completed Dir
-#	* Move .rpt files to Converted Dir
---
+++ src/FileCleaner.py
@@ -0,0 +1,5 @@
+
+
+# Used to:
+#   * Move Converted .ack files to Completed Dir
+#   * Move .rpt files to Converted Dir
